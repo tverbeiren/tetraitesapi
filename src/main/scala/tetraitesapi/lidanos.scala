9def7b742ea84b467a2fade76c6ceced328a6da8
Toni Verbeiren <toni.verbeiren@gmail.com>
2016-11-24 14:58:40 +0100

    lidanos endpoint added

---
+++ src/main/scala/tetraitesapi/lidanos.scala
@@ -0,0 +1,42 @@
+package tetraitesapi
+
+import com.typesafe.config.Config
+import org.apache.spark.SparkContext
+import spark.jobserver._
+import tetraitesapi.Model._
+
+import scala.util.Try
+
+object lidanos extends SparkJob with NamedObjectSupport {
+
+  implicit def rddPersister[T] : NamedObjectPersister[NamedRDD[T]] = new RDDPersister[T]
+  implicit def broadcastPersister[U] : NamedObjectPersister[NamedBroadcast[U]] = new BroadcastPersister[U]
+
+  override def validate(sc: SparkContext, config: Config): SparkJobValidation = SparkJobValid
+
+  override def runJob(sc: SparkContext, config: Config): Any = {
+
+    val lidanoQuery:String = Try(config.getString("lidano")).getOrElse("no lidano specified")
+    val dayQuery:String = Try(config.getString("day")).getOrElse("no day specified")
+
+    // Fetch raw events
+    val NamedRDD(gezoDb, _ ,_) = namedObjects.get[NamedRDD[Gezo]]("gezoDb").get
+    val NamedRDD(farmaDb, _ ,_) = namedObjects.get[NamedRDD[Farma]]("farmaDb").get
+
+    // Dictionary ATC codes
+    val NamedBroadcast(atcDictBc) = namedObjects.get[NamedBroadcast[scala.collection.Map[String,String]]]("atcDict").get
+    val atcDict = atcDictBc.value
+
+    // Dictionary Prestaties
+    val NamedBroadcast(prestDictBc) = namedObjects.get[NamedBroadcast[scala.collection.Map[String,String]]]("prestDict").get
+    val prestDict = prestDictBc.value
+
+    // Stats
+    val listLidano = (gezoDb.map(_.lidano).collect.toList ++ farmaDb.map(_.lidano).collect.toList).distinct
+
+    Map("meta" -> s"List of lidanos in the database") ++
+      Map("data" -> listLidano)
+
+  }
+
+}
